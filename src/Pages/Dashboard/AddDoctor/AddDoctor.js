968dd53874916bc6b9cb16e4238de1c020159f50
My Name <cpgsom819@gmail.com>
2023-04-21 21:03:38 +0800

    manage doctor adeed

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -38,7 +38,7 @@ const AddDoctor = () => {
             formData.append('image' , image);
 
 
-            const url =`https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`
+            const url =`https://api.imgbb.com/1/upload?key=${imageHostKey}`
 
 
             fetch(url, {
--- src/Pages/Dashboard/AllUser/ManageDoctor.js
+++ src/Pages/Dashboard/AllUser/ManageDoctor.js
@@ -1,13 +1,80 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const ManageDoctor = () => {
+
+    const url = `http://localhost:5000/doctors`
+
+    const { data : doctors = []} = useQuery({
+        queryKey : ['doctors'],
+        queryFn : async () => {
+            try {
+                const res =  await fetch(url, {
+                    headers : {
+                        authorization : `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                })
+                const data = await res.json();
+                return data; 
+            }
+            catch(error){
+
+            }
+        }
+
+
+    })
+
+
+
+
     return (
-        <div>
-            
-            <h1>ManageDoctor </h1>
 
+        <div>            
+            <h1>ManageDoctor  {doctors?.length} </h1>
 
+            <div className="overflow-x-auto">
+  <table className="table w-full">
+    {/* head*/}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Avatar</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Specialty</th>
+        <th>Action</th>
 
+      </tr>
+    </thead>
+    <tbody>
+  
+
+   {
+    doctors?.map((doctor, i) =>     
+    
+    <tr >
+    <th>{i+1}</th>
+    <td>
+    <div className="avatar">
+  <div className="w-10 rounded">
+    <img src={doctor.image}  alt=''/>
+  </div>
+</div>
+    </td>
+    <td>{doctor.name}</td>
+    <td>{doctor.email}</td>
+    <td>{doctor.specialty}</td>
+    <th><button className="btn btn-outline btn-error">Delete </button> </th>
+   
+  </tr>)
+   }
+   
+
+     
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
