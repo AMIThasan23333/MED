835604b64332d19fa148e3adc8fbe8e6703727d8
My Name <cpgsom819@gmail.com>
2023-04-22 01:47:44 +0800

    delete done

--- src/Pages/Dashboard/AllUser/ManageDoctor.js
+++ src/Pages/Dashboard/AllUser/ManageDoctor.js
@@ -1,113 +1,106 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { useState } from 'react';
-import ConfirmationModal from '../../Shared/ConfirmationModal/ConfirmationModal';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+// import ConfirmationModal from '../../Shared/ConfirmationModal/ConfirmationModal';
+// import Loading from '../../Shared/Loading/Loading';
+import Loading from './../../Shared/Loading/Loading';
+import ConfirmationModal from './../../Shared/ConfirmationModal/ConfirmationModal';
 
-const ManageDoctor = () => {
-
-
-
-    const [DeletingDoctor, setDeletingDoctor] = useState(null);
+const ManageDoctors = () => {
+    const [deletingDoctor, setDeletingDoctor] = useState(null);
 
     const closeModal = () => {
         setDeletingDoctor(null);
     }
 
-    const handleDeleteDoctor = doctor => {
-        console.log(doctor)
-    }
 
-    const url = `http://localhost:5000/doctors`
-    const { data : doctors = []} = useQuery({
-        queryKey : ['doctors'],
-        queryFn : async () => {
+    const { data: doctors, isLoading, refetch } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
             try {
-                const res =  await fetch(url, {
-                    headers : {
-                        authorization : `bearer ${localStorage.getItem('accessToken')}`
+                const res = await fetch('http://localhost:5000/doctors', {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
                     }
-                })
+                });
                 const data = await res.json();
-                return data; 
+                return data;
             }
-            catch(error){
+            catch (error) {
 
             }
         }
+    });
 
+    
+    const handleDeleteDoctor = doctor => {
+        fetch(`http://localhost:5000/doctors/${doctor._id}`, {
+            method: 'DELETE', 
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                refetch();
+                toast.success(`Doctor ${doctor.name} deleted successfully`)
+            }
+        })
+    }
 
-    })
-
-
-
+    if (isLoading) {
+        return <Loading></Loading>
+    }
 
     return (
-
-        <div>            
-            <h1>ManageDoctor  {doctors?.length} </h1>
-
+        <div>
+            <h2 className="text-3xl">Manage Doctors: {doctors?.length}</h2>
             <div className="overflow-x-auto">
-  <table className="table w-full">
-    {/* head*/}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Avatar</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Specialty</th>
-        <th>Action</th>
-
-      </tr>
-    </thead>
-    <tbody>
-  
-
-   {
-    doctors?.map((doctor, i) =>     
-    
-    <tr >
-    <th>{i+1}</th>
-    <td>
-    <div className="avatar">
-  <div className="w-10 rounded">
-    <img src={doctor.image}  alt=''/>
-  </div>
-</div>
-
-
-    </td>
-    <td>{doctor.name}</td>
-    <td>{doctor.email}</td>
-    <td>{doctor.specialty}</td>
-    <th>
-    <label  onClick={() =>  setDeletingDoctor(doctor)} htmlFor="my-modal"  className="btn btn-outline btn-error">Delete</label>
-       </th>
-  </tr>)
-   }
-       
-    </tbody>
-  </table>
-</div>
-               
-               {
-                DeletingDoctor && <ConfirmationModal
-
-                title= {` Are You sure you want to delete`}
-                message= {`IF you delete ${DeletingDoctor.name} It can not be undone`}
-                closeModal={closeModal}
-                 modalData={DeletingDoctor}
-                successAction={handleDeleteDoctor}
-                
-                successButtonName="Delete"
-                
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, i) => <tr key={doctor._id}>
+                                <th>{i + 1}</th>
+                                <td><div className="avatar">
+                                    <div className="w-24 rounded-full">
+                                        <img src={doctor.image} alt="" />
+                                    </div>
+                                </div></td>
+                                <td>{doctor.name}</td>
+                                <td>{doctor.email}</td>
+                                <td>{doctor.specialty}</td>
+                                <td>
+                                    <label onClick={() => setDeletingDoctor(doctor)} htmlFor="confirmation-modal" className="btn btn-sm btn-error">Delete</label>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {
+                deletingDoctor && <ConfirmationModal
+                    title={`Are you sure you want to delete?`}
+                    message={`If you delete ${deletingDoctor.name}. It cannot be undone.`}
+                    successAction = {handleDeleteDoctor}
+                    successButtonName="Delete"
+                    modalData = {deletingDoctor}
+                    closeModal = {closeModal}
                 >
-                 
                 </ConfirmationModal>
-               }
-
+            }
         </div>
     );
 };
 
-export default ManageDoctor;
+export default ManageDoctors;
